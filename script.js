45d6e0d51ac616cfa705a5de8bb021b0ede194b8
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-07-09 14:16:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-//your code here
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Pink Floyd', 'Radiohead'];
+
+function sortBandNames(names) {
+  const articles = ['a', 'an', 'the'];
+
+  // Remove articles from band names
+  const sortedNames = names.map((name) => {
+    const words = name.toLowerCase().split(' ');
+    const filteredWords = words.filter((word) => !articles.includes(word));
+    return filteredWords.join(' ');
+  });
+
+  // Sort band names in lexicographic order
+  sortedNames.sort();
+
+  return sortedNames;
+}
+
+function displayBandNames(names) {
+  const bandList = document.getElementById('band');
+  names.forEach((name) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = name;
+    bandList.appendChild(listItem);
+  });
+}
+
+const sortedBandNames = sortBandNames(bandNames);
+displayBandNames(sortedBandNames);
+
 
